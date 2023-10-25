1ea9a692a66748273ae354b2b88eb59efda5a0a0
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-10-25 15:21:18 -0300

    fixed some things

--- HealthSystemNew/Program.cs
+++ HealthSystemNew/Program.cs
@@ -330,7 +330,7 @@ namespace HealthSystem
             }
 
         }
-        static void ColorControl(int color, int sColor)
+        static void ColorControl(ConsoleColor color, int sColor)
         {
             if (color == 0)
             {
@@ -363,91 +363,22 @@ namespace HealthSystem
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
-        static void ColorString(int color, string fColor)
+        static void ColorString(ConsoleColor color, string fColor)
         {
-            if (color == 0)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.ForegroundColor = color;
                 Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 1)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 2)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 3)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 4)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 5)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 6)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 7)
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 8)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 9)
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 10)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (color == 11)
-            {
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.Write(fColor);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
+                Console.ForegroundColor = ConsoleColor.White;   
         }
         static void HealthBar()
         {
             if (health <= 0)
             {
                 Console.Write("\nHealth: ");
-                ColorControl(1, 0);
+                ColorControl(ConsoleColor.Red, 0);
                 Console.Write(" (");
-                ColorControl(1, health);
+                ColorControl(ConsoleColor.Red, health);
                 Console.Write(")\nStatus: ");
-                ColorString(0, "Dead\n");
+                ColorString(ConsoleColor.DarkRed, "Dead\n");
                 health = 100;
                 shield = 100;
                 lives--;
@@ -457,48 +388,48 @@ namespace HealthSystem
                 if (lives != 0)
                 {
                     Console.Write("Shield: ");
-                    ColorControl(0, shield);
-                    ColorString(6, "\nShield Full");
+                    ColorControl(ConsoleColor.Cyan, shield);
+                    ColorString(ConsoleColor.DarkBlue, "\nShield Full");
                     Console.Write("\nHealth: ");
-                    ColorControl(1, health);
+                    ColorControl(ConsoleColor.Red, health);
                     Console.Write("\nStatus: ");
-                    ColorString(1, "Perfect Health");
+                    ColorString(ConsoleColor.DarkGreen, "Perfect Health");
                 }
             }
             else if (health > 0 && health <= 10)
             {
                 Console.Write("\nHealth: ");
-                ColorControl(1, health);
+                ColorControl(ConsoleColor.Red, health);
                 Console.Write("\nStatus: ");
-                ColorString(5, "Imminent Danger");
+                ColorString(ConsoleColor.Red, "Imminent Danger");
             }
             else if (health > 10 && health <= 50)
             {
                 Console.Write("\nHealth: ");
-                ColorControl(1, health);
+                ColorControl(ConsoleColor.Red, health);
                 Console.Write("\nStatus: ");
-                ColorString(4, "Badly Hurt");
+                ColorString(ConsoleColor.Yellow, "Badly Hurt");
             }
             else if (health > 50 && health <= 75)
             {
                 Console.Write("\nHealth: ");
-                ColorControl(1, health);
+                ColorControl(ConsoleColor.Red, health);
                 Console.Write("\nStatus: ");
-                ColorString(3, "Hurt");
+                ColorString(ConsoleColor.DarkYellow, "Hurt");
             }
             else if (health > 75 && health < 100)
             {
                 Console.Write("\nHealth: ");
-                ColorControl(1, health);
+                ColorControl(ConsoleColor.Red, health);
                 Console.Write("\nStatus: ");
-                ColorString(2, "Healthy");
+                ColorString(ConsoleColor.Green, "Healthy");
             }
             else if (health == 100)
             {
                 Console.Write("\nHealth: ");
-                ColorControl(1, health);
+                ColorControl(ConsoleColor.Red, health);
                 Console.Write("\nStatus: ");
-                ColorString(1, "Perfect Health");
+                ColorString(ConsoleColor.DarkGreen, "Perfect Health");
             }
         }
         static void ShieldStatus()
